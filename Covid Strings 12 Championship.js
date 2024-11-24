f2ce25a05bc8102b25bd9529070d0df905dcff72
SGTIBALCOVIDSTRINGS <stephen.tiballi@daemen.edu>
2024-11-23 19:20:11 -0500

    Create Covid Strings 12 Championship.js
    ready for readme.

---
+++ Covid Strings 12 Championship.js
@@ -0,0 +1,32 @@
+doc.type.html
+script
+html
+head
+filename=covid strings12 championship.js
+team low seed =lowest remaining seed
+team high seed= highest remaining seed
+championship game=( highest and lowest remaining seeds ) 
+createround covid strings12 championship round 
+round = document.createelement(div);
+round.classlist.add(round);
+for (let i =zero; i < teams.length; i += two) 
+round.appendchild(creatematchup(teams[i],teams[i+one]));
+return round;
+java util.scanner
+java.util
+questions[]questions={
+question26
+question27
+question28
+question29
+question30
+question31
+question32
+question33
+advanceteams=slice(currentteams)
+result=declarewinner(team1 score, team2 score)
+if team high seed score>=sixty:
+console.log(highest remaining seed wins covid strings12)
+else:
+console.log(lowest remaining seed wins covid strings12)
+return filename
